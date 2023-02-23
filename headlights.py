6ac9648b5ef6e8b9815e5b107db382f6b26cd7d5
Coyote21 <hizuko@me.com>
2023-02-24 00:40:30 +1100

    Added sphero_sdk

--- headlights.py
+++ headlights.py
@@ -4,7 +4,8 @@ import time
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../../../')))
 
-from sphero_sdk import RvrStreamingServices, SpheroRvrObserver
+from sphero_sdk import RvrStreamingServices
+from sphero_sdk import SpheroRvrObserver
 from sphero_sdk import Colors
 from sphero_sdk import RvrLedGroups
 
