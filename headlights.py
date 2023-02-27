89ef8049040893d971239d3a2d4038dd73897d9b
Coyote21 <hizuko@me.com>
2023-02-27 20:02:49 +1100

    Reformatted to use scan, think, act functions

--- headlights.py
+++ headlights.py
@@ -12,32 +12,33 @@ from sphero_sdk import RvrLedGroups
 rvr = SpheroRvrObserver()
 
 SCALAR = 0.002125           # Scalar to map sensor data to RGB values
+ambient_light_data = 0.0
 headlight_brightness = 0
 
 # Ambient Light Sensor Data Handler
-def ambient_light_handler(ambient_light_data):
+def scan(ambient_light_data):
     print('Ambient light sensor data response: ', ambient_light_data)
     
-    # Calculate Headlight Brightness
+# Calculate Headlight Brightness
+def think():
     headlight_brightness = 255 - int(ambient_light_data*SCALAR)
     print('Headlight brightness:', headlight_brightness)
+
+# Set the Headlight Brightness
+def act():
+    rgb_tuple = [headlight_brightness, 
+                 headlight_brightness, 
+                 headlight_brightness]
     
-    # Set the Headlight Brightness
+    # Set Left Headlight
     rvr.set_all_leds(
         led_group=RvrLedGroups.headlight_left.value,
-        led_brightness_values=[
-            headlight_brightness, 
-            headlight_brightness, 
-            headlight_brightness
-            ]
+        led_brightness_values=rgb_tuple
     )
+    # Set Right Headlight
     rvr.set_all_leds(
         led_group=RvrLedGroups.headlight_right.value,
-        led_brightness_values=[
-            headlight_brightness, 
-            headlight_brightness, 
-            headlight_brightness
-            ]
+        led_brightness_values=rgb_tuple
     )
 
 def main():
@@ -48,18 +49,20 @@ def main():
         # Give RVR time to wake up
         time.sleep(2)
 
-        # Assign Ambient Light Sensor to Handler
+        # Assign ambient light sensor to handler
         rvr.sensor_control.add_sensor_data_handler(
             service=RvrStreamingServices.ambient_light,
-            handler=ambient_light_handler
+            handler=scan
         )
 
-        # Start reading Sensor Data @ 250ms intervals
-        rvr.sensor_control.start(interval=250)
+        # Start reading sensor data @ 300ms intervals
+        rvr.sensor_control.start(interval=300)
 
         while True:
-            # Delay to allow RVR to stream sensor data
-            time.sleep(1)
+            # Delay to allow RVR to stream ambient light sensor data
+            time.sleep(0.3)
+            think()
+            act()
 
     # End loop/program with Ctrl+c, Ctrl+z or Delete
     except KeyboardInterrupt:       
